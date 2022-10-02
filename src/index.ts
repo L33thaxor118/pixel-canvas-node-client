d750555ac7099ac58a6ee73342198623bbecc1b3
Azriel Alvarado <alvarado_118@yahoo.com>
2022-10-02 14:36:03 -0500

    Rename types and clean up

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,32 @@
 import WebSocket from 'ws'
-
-interface PaintCommand {
-  x: number,
-  y: number,
-  color: number
-}
+const socket: WebSocket = new WebSocket('wss://282bm5cqa4.execute-api.us-east-1.amazonaws.com/development/')
 
 interface AWSRequest {
   action: string
 }
 
-/**
- * Represents canvas state in 2 dimensions
- */
-type MatrixState = Array<Array<number>>
+interface PaintCommand extends AWSRequest {
+  x: number,
+  y: number,
+  color: number
+}
 
-const socket: WebSocket = new WebSocket('wss://282bm5cqa4.execute-api.us-east-1.amazonaws.com/development/')
+// Represents canvas state in 2 dimensions
+type CanvasState = Array<Array<number>>
 
-type MatrixStateListener = (event: MatrixState)=>void
+type CanvasStateListener = (state: CanvasState) => void
 
-var stateListener: MatrixStateListener | null = null
+var stateListener: CanvasStateListener | null = null
 
-var currentState: MatrixState
+// Assume canvas is empty until we receive state from server
+var currentState: CanvasState = Array(16).fill(0).map(_ => Array(16).fill(0))
+
+socket.on('open', () => {
+  const request: AWSRequest = {
+    action: "get"
+  }
+  socket.send(JSON.stringify(request))
+})
 
 socket.on('message', (data) => {
   const messageData = JSON.parse(data.toString())
@@ -37,21 +42,15 @@ socket.on('message', (data) => {
   }
 })
 
-socket.on('open', () => {
-  const request: AWSRequest = {
-    action: "get"
-  }
-  socket.send(JSON.stringify(request))
-})
-
 /**
- * Paints a pixel on the matrix
+ * Paints a pixel on the canvas
  * @param x - x position of pixel to paint, assuming x=0,y=0 is top-left corner
  * @param y - y position of pixel to paint, assuming x=0,y=0 is top-left corner
  * @param color - decimal representation of the color to paint
  */
-function paintTile(x: number, y: number, color: number) {
+function paintPixel(x: number, y: number, color: number) {
   const message: PaintCommand = {
+    action: "paint",
     x: x,
     y: y,
     color: color
@@ -59,11 +58,12 @@ function paintTile(x: number, y: number, color: number) {
   socket.send(JSON.stringify(message))
 }
 
-function setStateListener(listener: MatrixStateListener) {
+function setStateListener(listener: CanvasStateListener) {
   stateListener = listener
+  listener(currentState)
 }
 
 export default {
-  paintTile,
+  paintPixel,
   setStateListener
 }
